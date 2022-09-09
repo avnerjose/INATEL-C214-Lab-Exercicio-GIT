9b8334270916aaa47db6ab32c7255696f54c9956
avnerjose <avnerj.g.r@gmail.com>
2022-09-09 09:32:17 -0300

    Ao clicar em um post na lista mostra o conteúdo do body

--- lib/view/components/post_list_item.dart
+++ lib/view/components/post_list_item.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class PostListItem extends StatelessWidget {
+class PostListItem extends StatefulWidget {
   final int id;
   final int userId;
   final String title;
@@ -15,37 +15,56 @@ class PostListItem extends StatelessWidget {
       : super(key: key);
 
   @override
+  State<PostListItem> createState() => _PostListItemState();
+}
+
+class _PostListItemState extends State<PostListItem> {
+  bool _isExpanded = false;
+
+  @override
   Widget build(BuildContext context) {
     return (Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(children: [
-          Container(
-              decoration: BoxDecoration(
-                  color: Colors.blue, borderRadius: BorderRadius.circular(100)),
-              height: 40,
-              width: 40,
-              child: Center(
-                child: Text(
-                  id.toString(),
-                  style: const TextStyle(color: Colors.white, fontSize: 20),
-                ),
-              )),
-          const SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text("User Id: $userId"),
-                Text(
-                  "Title: $title",
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
+      child: InkWell(
+        onTap: () {
+          setState(() {
+            _isExpanded = !_isExpanded;
+          });
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(children: [
+            Container(
+                decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(100)),
+                height: 40,
+                width: 40,
+                child: Center(
+                  child: Text(
+                    widget.id.toString(),
+                    style: const TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                )),
+            const SizedBox(width: 10),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text("User Id: ${widget.userId}"),
+                  Text(
+                    "Title: ${widget.title}",
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  if (_isExpanded) ...[
+                    const SizedBox(height: 10),
+                    Text(widget.body)
+                  ]
+                ],
+              ),
             ),
-          ),
-        ]),
+          ]),
+        ),
       ),
     ));
   }
